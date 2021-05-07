7e7a3e5b84d1c946c1c2373a558c81307ad9b834
PinPex <aleksei-picnhuk0202@mail.ru>
2021-05-07 12:52:57 +0700

    Realised weight and time

--- src/libconverter/converter.cpp
+++ src/libconverter/converter.cpp
@@ -48,4 +48,39 @@ double Metres_Inch(double parametr, bool mable) {
 double Metres_LightYear(double parametr, bool mable) {
     return exp_ten(parametr, -16 * pow(-1, mable)) * pow(pow(-1, mable), 1.057008707);}
 
+double underpow(double basis, char n)
+{
+	if(n==-1) return 1/basis;
+	else return basis;
+}
+double kg_to_notkg(double a, string b, char n)
+{
+	if(b=="Carrat") return a*underpow(pow_ten(5,3),n);
+	if(b=="Short ton") return kg_to_notkg(a, "Carrat",0)/underpow(4535920,n);
+	if(b=="Ounce") return kg_to_notkg(a, "Short ton",0)*underpow(pow_ten(32,3),n);
+	if(b=="Pound") return kg_to_notkg(a, "Short ton",0)*underpow(pow_ten(2,3),n);
+	if(b=="Long ton") return kg_to_notkg(a, "Short ton",0)/underpow(1.2,n);
+	return 0;
+}
 
+double pow_sixty(double x, int a)
+{
+	while(a<0){
+		a+=1;
+		x/=60;
+	}
+	while(a>0){
+		a-=1;
+		x*=60;
+	}
+	return x;
+}
+
+double hour_to_nothour(double a, string b, int n)
+{
+	if(b=="Day") return underpow(a/24,n);
+	if(b=="Week") return underpow(hour_to_nothour(a, "Day")/7,n);
+	if(b=="Month") return underpow((hour_to_nothour(a, "Day")*24)/731,n);
+	if(b=="Year") return underpow(hour_to_nothour(a, "Month")/12,n);
+	return 0;
+}
