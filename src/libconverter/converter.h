bd208c93c4ce3e4c6a43c4a967cd612ad41b5175
PinPex <aleksei-picnhuk0202@mail.ru>
2021-05-07 13:11:31 +0700

    Tryign follow clang

--- src/libconverter/converter.h
+++ src/libconverter/converter.h
@@ -1,20 +1,19 @@
 #pragma once
 
-double Kelvin_Celsius(double parametr, bool mable);
+double Kelvin_Celsius (double parametr, bool mable);
 
-double In_Fahrenheit(double parametr, bool temp);
+double In_Fahrenheit (double parametr, bool temp);
 
-double From_Fahrenheit(double parametr, bool temp);
+double From_Fahrenheit (double parametr, bool temp);
 
-double exp_ten(double parametr, int degree);
+double exp_ten (double parametr, int degree);
 
-double Metres_Mile(double parametr, bool mable);
+double Metres_Mile (double parametr, bool mable);
 
-double Metres_Yard(double parametr, bool mable);
+double Metres_Yard (double parametr, bool mable);
 
-double Metres_Foot(double parametr, bool mable);
+double Metres_Foot (double parametr, bool mable);
 
-double Metres_Inch(double parametr, bool mable);
+double Metres_Inch (double parametr, bool mable);
 
-double Metres_LightYear(double parametr, bool mable);
-
+double Metres_LightYear (double parametr, bool mable);
