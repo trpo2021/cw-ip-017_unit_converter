a11eae6ba4fef0b36975d8f6677ff1694493b25e
AlexseiT <alex.todorov@mail.ru>
2021-05-09 15:26:37 +0700

    Lib clang format

--- src/libconverter/converter.h
+++ src/libconverter/converter.h
@@ -1,20 +1,26 @@
 #ifndef CONVERTER_LIB
 #define CONVERTER_LIB
-
 #include <gtk/gtk.h>
 #include <sstream>
 #include <string>
 #include <vector>
 using namespace std;
 
-void weight_calc(vector<double> &elements, double input_element,
-                 int position_of_element);
+void weight_calc(
+        vector<double>& elements,
+        double input_element,
+        int position_of_element);
 
-void from_double_to_char(vector<double> elements, int i,
-                         const gchar *input_char, GtkEntry *output);
+void from_double_to_char(
+        vector<double> elements,
+        int i,
+        const gchar* input_char,
+        GtkEntry* output);
 
-void time(vector<double> &elements, double input_element,
-          int position_of_element);
+void time(
+        vector<double>& elements,
+        double input_element,
+        int position_of_element);
 
 double Kelvin_Celsius(double parametr, bool mable);
 
@@ -22,10 +28,14 @@ double In_Fahrenheit(double parametr, bool temp);
 
 double From_Fahrenheit(double parametr, bool temp);
 
-void length(vector<double> &elements, double input_element,
-            int position_of_element);
+void length(
+        vector<double>& elements,
+        double input_element,
+        int position_of_element);
 
-void temp(vector<double> &elements, double input_element,
-          int position_of_element);
+void temp(
+        vector<double>& elements,
+        double input_element,
+        int position_of_element);
 
 #endif
