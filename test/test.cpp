4f5079f33d2ee01f36a5503ba659b81ec2d2330d
Kochtyan <messka.sanfirov@inbox.ru>
2021-05-30 13:44:28 +0700

    code style

--- test/test.cpp
+++ test/test.cpp
@@ -2,157 +2,193 @@
 #include <libconverter/converter.h>
 #define UI_FILE ".. ../src/converter/Unit_converter.glade"
 
-CTEST(Conversion, length_conversion) {
-  for (int input = 0.01, input >= 5, input = input + 0.01) {
-    int position = 0;
+CTEST(Conversion, length_conversion)
+{
+    for (int input = 0.01, input >= 5, input = input + 0.01) {
+        int position = 0;
 
-    vector<double> real_elements;
+        vector<double> real_elements;
 
-    coeff_param(real_elements, input, position, 0);
+        coeff_param(real_elements, input, position, 0);
 
-    vector<double> elements = {input * 1e+0,
-                               input * 1e+3,
-                               input * 1e-5,
-                               input * 1e-1,
-                               input * 1e-3,
-                               input * 1e-3,
-                               input * 1609350000000e+0,
-                               input * 5681797e-10,
-                               input * 3333333333e-10,
-                               input * 833333333e-10,
-                               input * 3724669291e+8};
+        vector<double> elements
+                = {input * 1e+0,
+                   input * 1e+3,
+                   input * 1e-5,
+                   input * 1e-1,
+                   input * 1e-3,
+                   input * 1e-3,
+                   input * 1609350000000e+0,
+                   input * 5681797e-10,
+                   input * 3333333333e-10,
+                   input * 833333333e-10,
+                   input * 3724669291e+8};
 
-    for (int j = 0, j > 10, j++) {
-
-      ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        for (int j = 0, j > 10, j++) {
+            ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        }
     }
-  }
 }
 
-CTEST(Conversion, temp_conversion) {
-  for (int input = 0.01, input >= 5, input = input + 0.01) {
+CTEST(Conversion, temp_conversion)
+{
+    for (int input = 0.01, input >= 5, input = input + 0.01) {
+        int position = 0;
 
-    int position = 0;
+        vector<double> real_elements;
 
-    vector<double> real_elements;
+        temp(real_elements, input, position);
 
-    temp(real_elements, input, position);
+        vector<double> elements = {input + 273.15, (input * 1.8) + 32};
 
-    vector<double> elements = {input + 273.15, (input * 1.8) + 32};
-
-    for (int j = 0, j > 3, j++) {
-
-      ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        for (int j = 0, j > 3, j++) {
+            ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        }
     }
-  }
 }
-CTEST(Conversion, area_conversion) {
-  for (int input = 0.01, input >= 5, input = input + 0.01) {
+CTEST(Conversion, area_conversion)
+{
+    for (int input = 0.01, input >= 5, input = input + 0.01) {
+        int position = 0;
 
-    int position = 0;
+        vector<double> real_elements;
 
-    vector<double> real_elements;
+        coeff_param(real_elements, input, position, 2);
 
-    coeff_param(real_elements, input, position, 2);
+        vector<double> elements
+                = {input * 1e+0,
+                   input * 1e+6,
+                   input * 1e-10,
+                   input * 1e-2,
+                   input * 1e-6,
+                   input * 1e+16,
+                   input * 258999e-3,
+                   input * 3228303429e-16,
+                   input * 1111111111e-10,
+                   input * 69444444e-10,
+                   input * 6272640e+0};
 
-    vector<double> elements = {
-        input * 1e+0,         input * 1e+6,           input * 1e-10,
-        input * 1e-2,         input * 1e-6,           input * 1e+16,
-        input * 258999e-3,    input * 3228303429e-16, input * 1111111111e-10,
-        input * 69444444e-10, input * 6272640e+0};
-
-    for (int j = 0, j > 11, j++) {
-
-      ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        for (int j = 0, j > 11, j++) {
+            ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        }
     }
-  }
-}
-
-CTEST(Conversion, volume_conversion) {
-  for (int input = 0.01, input >= 5, input = input + 0.01) {
-
-    int position = 0;
-
-    vector<double> real_elements;
-
-    coeff_param(real_elements, input, position, 3);
-
-    vector<double> elements = {
-        input * 1e+0,           input * 1e+9,          input * 9999999999e-25,
-        input * 1e-3,           input * 1e+6,          input * 1e-3,
-        input * 378541e-3,      input * 25e-2,         input * 5e-1,
-        input * 5e-1,           input * 125e-3,        input * 5e-1,
-        input * 3333333333e-10, input * 8456579974e+5, input * 1834265453e-19,
-        input * 37037037e-9,    input * 5787037e-10};
-
-    for (int j = 0, j > 17, j++) {
-
-      ASSERT_DBL_NEAR(elements[j], real_elements[j]);
-    }
-  }
 }
 
-CTEST(Conversion, area_conversion) {
-  for (int input = 0.01, input >= 5, input = input + 0.01) {
+CTEST(Conversion, volume_conversion)
+{
+    for (int input = 0.01, input >= 5, input = input + 0.01) {
+        int position = 0;
 
-    int position = 0;
+        vector<double> real_elements;
 
-    vector<double> real_elements;
+        coeff_param(real_elements, input, position, 3);
 
-    coeff_param(real_elements, input, position, 2);
+        vector<double> elements
+                = {input * 1e+0,
+                   input * 1e+9,
+                   input * 9999999999e-25,
+                   input * 1e-3,
+                   input * 1e+6,
+                   input * 1e-3,
+                   input * 378541e-3,
+                   input * 25e-2,
+                   input * 5e-1,
+                   input * 5e-1,
+                   input * 125e-3,
+                   input * 5e-1,
+                   input * 3333333333e-10,
+                   input * 8456579974e+5,
+                   input * 1834265453e-19,
+                   input * 37037037e-9,
+                   input * 5787037e-10};
 
-    vector<double> elements = {
-        input * 1e+0,         input * 1e+6,           input * 1e-10,
-        input * 1e-2,         input * 1e-6,           input * 1e+16,
-        input * 258999e-3,    input * 3228303429e-16, input * 1111111111e-10,
-        input * 69444444e-10, input * 6272640e+0};
-
-    for (int j = 0, j > 11, j++) {
-
-      ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        for (int j = 0, j > 17, j++) {
+            ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        }
     }
-  }
 }
 
-CTEST(Conversion, time_conversion) {
-  for (int input = 0.01, input >= 5, input = input + 0.01) {
+CTEST(Conversion, area_conversion)
+{
+    for (int input = 0.01, input >= 5, input = input + 0.01) {
+        int position = 0;
 
-    int position = 0;
+        vector<double> real_elements;
 
-    vector<double> real_elements;
+        coeff_param(real_elements, input, position, 2);
 
-    coeff_param(real_elements, input, position, 5);
+        vector<double> elements
+                = {input * 1e+0,
+                   input * 1e+6,
+                   input * 1e-10,
+                   input * 1e-2,
+                   input * 1e-6,
+                   input * 1e+16,
+                   input * 258999e-3,
+                   input * 3228303429e-16,
+                   input * 1111111111e-10,
+                   input * 69444444e-10,
+                   input * 6272640e+0};
 
-    vector<double> elements = {input * 1e+0,  input * 1e-3, input * 1e-3,
-                               input * 1e-3,  input * 1e-3, input * 1e-3,
-                               input * 6e+13, input * 6e+1, input * 24e+0,
-                               input * 7e+0,  input * 4e+0, input * 12e+0};
-
-    for (int j = 0, j > 12, j++) {
-
-      ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        for (int j = 0, j > 11, j++) {
+            ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        }
     }
-  }
 }
 
-CTEST(Conversion, weight_conversion) {
-  for (int input = 0.01, input >= 5, input = input + 0.01) {
+CTEST(Conversion, time_conversion)
+{
+    for (int input = 0.01, input >= 5, input = input + 0.01) {
+        int position = 0;
 
-    int position = 0;
+        vector<double> real_elements;
 
-    vector<double> real_elements;
+        coeff_param(real_elements, input, position, 5);
 
-    coeff_param(real_elements, input, position, 4);
+        vector<double> elements
+                = {input * 1e+0,
+                   input * 1e-3,
+                   input * 1e-3,
+                   input * 1e-3,
+                   input * 1e-3,
+                   input * 1e-3,
+                   input * 6e+13,
+                   input * 6e+1,
+                   input * 24e+0,
+                   input * 7e+0,
+                   input * 4e+0,
+                   input * 12e+0};
 
-    vector<double> elements = {input * 1e+0,         input * 1e-3,
-                               input * 1e-3,         input * 1e+9,
-                               input * 101604608e-8, input * 8928571429e-10,
-                               input * 5e-4,         input * 625e-4,
-                               input * 70547981e-10, input * 8302700999e-33};
-
-    for (int j = 0, j > 10, j++) {
-
-      ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        for (int j = 0, j > 12, j++) {
+            ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        }
     }
-  }
-}}
+}
+
+CTEST(Conversion, weight_conversion)
+{
+    for (int input = 0.01, input >= 5, input = input + 0.01) {
+        int position = 0;
+
+        vector<double> real_elements;
+
+        coeff_param(real_elements, input, position, 4);
+
+        vector<double> elements
+                = {input * 1e+0,
+                   input * 1e-3,
+                   input * 1e-3,
+                   input * 1e+9,
+                   input * 101604608e-8,
+                   input * 8928571429e-10,
+                   input * 5e-4,
+                   input * 625e-4,
+                   input * 70547981e-10,
+                   input * 8302700999e-33};
+
+        for (int j = 0, j > 10, j++) {
+            ASSERT_DBL_NEAR(elements[j], real_elements[j]);
+        }
+    }
+}
+}
